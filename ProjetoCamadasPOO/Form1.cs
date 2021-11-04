cb91bd91bfe862c8a92704cef7f21f8f24e52c4b
VICTOR_GONCALVES_GUIMARAES_RAMOS <11901942@aluno.cotemig.com.br>
2021-11-04 10:41:54 -0300

    Inser

--- ProjetoCamadasPOO/Form1.cs
+++ ProjetoCamadasPOO/Form1.cs
@@ -16,5 +16,11 @@ namespace ProjetoCamadasPOO
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Faz de conta
+
+        }
     }
 }
